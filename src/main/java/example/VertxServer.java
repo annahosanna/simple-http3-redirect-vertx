eab43ab9e3a392c1a0c32bb939b572617fc5326a
annahosanna <annahosanna@gmail.com>
2025-03-13 14:07:02 -0400

    Just need to start the server now

--- src/main/java/example/VertxServer.java
+++ src/main/java/example/VertxServer.java
@@ -3,7 +3,7 @@ package example;
 import io.vertx.core.Future;
 import io.vertx.core.VerticleBase;
 import io.vertx.ext.web.Router;
-import io.vertx.launcher.application.VertxApplication;
+// import io.vertx.
 
 /*
  * @author <a href="http://tfox.org">Tim Fox</a>
@@ -11,7 +11,7 @@ import io.vertx.launcher.application.VertxApplication;
 public class VertxServer extends VerticleBase {
 
   public static void main(String[] args) {
-    VertxApplication.main(new String[] { Server.class.getName() });
+   //  VertxApplication.main(new String[] { Server.class.getName() });
   }
 
   @Override
@@ -23,18 +23,17 @@ public class VertxServer extends VerticleBase {
     String headerMiddleText = new String("h3=");
     String headerEndText = new String(":8443");
     String quote = new String("\"");
+    // Some really weird problem where I couldn't escape quotes
+    String concatValue = new String(headerMiddleText.concat(quote).concat(headerEndText).concat(quote)); 
     router
       .route()
       .handler(routingContext -> {
         routingContext
           .response()
           .putHeader("Content-Type", "text/plain")
-          .putHeader("Content-Length", responseText.length())
-          .putHeader(
-            "Alt-Svc",
-            headerMiddleText.concat(quote).concat(headerEndText).concat(quote)
-          )
-          .end("Just a test");
+          .putHeader("Content-Length", String.valueOf(responseText.length()))
+          .putHeader("Alt-Svc", concatValue)
+          .end(responseText);
       });
 
     return vertx.createHttpServer().requestHandler(router).listen(8080);
