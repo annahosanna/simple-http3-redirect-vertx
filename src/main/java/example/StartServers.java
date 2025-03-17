4755bed5f3923b9ce4915c95553b4e41cd79b8fc
annahosanna <annahosanna@gmail.com>
2025-03-17 16:14:19 -0400

    This is basically examples from the vertx site

--- src/main/java/example/StartServers.java
+++ src/main/java/example/StartServers.java
@@ -1,22 +1,21 @@
 package example;
 
 import io.vertx.core.*;
-import io.vertx.core.*;
 import io.vertx.core.Future;
 import io.vertx.core.VerticleBase;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
-import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
-import io.vertx.launcher.application.VertxApplication;
-import io.vertx.launcher.application.VertxApplication;
+// import io.vertx.launcher.application.VertxApplication;
+// import io.vertx.launcher.application.VertxApplication;
 
 /*
  * @author <a href="http://tfox.org">Tim Fox</a>
  */
-public class DeployExample extends VerticleBase {
+public class StartServers extends VerticleBase {
 
   public static void main(String[] args) {
+	  // This looks like its calling main 
     VertxApplication.main(new String[] { DeployExample.class.getName() });
   }
 
@@ -26,38 +25,9 @@ public class DeployExample extends VerticleBase {
     vertx.deployVerticle("example.HttpVertical");
     System.out.println("Deployed HTTP Verticle");
     System.out.println("Deploying HTTPS Verticle");
-    vertx
-      .deployVerticle("example.HttpVertical")
-      .compose(deploymentID -> {
-        System.out.println(
-          "Other verticle deployed ok, deploymentID = " + deploymentID
-        );
+    vertx.deployVerticle("example.HttpsVertical");
+    System.out.println("Deployed HTTPS Verticle");
 
-        return vertx
-          .undeploy(deploymentID)
-          .onSuccess(res2 -> {
-            System.out.println("Verticles were indeployed");
-          });
-      });
-
-    // Deploy specifying some config
-    JsonObject config = new JsonObject().put("foo", "bar");
-    vertx.deployVerticle(
-      "io.vertx.example.core.verticle.deploy.OtherVerticle",
-      new DeploymentOptions().setConfig(config)
-    );
-
-    // Deploy 10 instances
-    vertx.deployVerticle(
-      "io.vertx.example.core.verticle.deploy.OtherVerticle",
-      new DeploymentOptions().setInstances(10)
-    );
-
-    // Deploy it as a worker verticle
-    vertx.deployVerticle(
-      "io.vertx.example.core.verticle.deploy.OtherVerticle",
-      new DeploymentOptions().setThreadingModel(ThreadingModel.WORKER)
-    );
 
     return super.start();
   }
